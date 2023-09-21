7e7e6b8759a83a8d8211ad4e89e3d261cbd7839d
Aurelien-jcb <jacob-aurelien@hotmail.fr>
2023-09-21 16:33:06 +0200

    implement collection page and update details page

---
+++ public/main.88a0f5f903ee40b84360.hot-update.js
@@ -0,0 +1,11 @@
+"use strict";
+self["webpackHotUpdatefloema_project"]("main",{},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("21d57638997688800cc9")
+/******/ })();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi44OGEwZjVmOTAzZWU0MGI4NDM2MC5ob3QtdXBkYXRlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O1VBQUEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9mbG9lbWEtcHJvamVjdC93ZWJwYWNrL3J1bnRpbWUvZ2V0RnVsbEhhc2giXSwic291cmNlc0NvbnRlbnQiOlsiX193ZWJwYWNrX3JlcXVpcmVfXy5oID0gKCkgPT4gKFwiMjFkNTc2Mzg5OTc2ODg4MDBjYzlcIikiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
